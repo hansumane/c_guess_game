cfa0e5114d992dabfc70ba0d79cfb7d04ba35b53
Creasure <yurxo@bk.ru>
2022-07-14 15:54:10 +0300

    purified source files

--- src/main.c
+++ src/main.c
@@ -3,18 +3,20 @@
 
 int main(void)
 {
-	int guess, value = true_random(1, 100);
+  int guess, value = true_random(1, 100);
 
-	do {
-		printf("Guess the value between 1 and 100: ");
-		scanf("%d", &guess); getchar();
-		if (guess > value)
-			printf("Your guess is bigger >!\n");
-		else if (guess < value)
-			printf("Your guess is smaller <!\n");
-		else
-			printf("Congratulations! You have guessed %u!\n", value);
-	} while (guess != value);
+  do
+    {
+      printf("Guess the value between 1 and 100: ");
+      scanf("%d", &guess); getchar();
+      if (guess > value)
+        printf("Your guess is bigger >!\n");
+      else if (guess < value)
+        printf("Your guess is smaller <!\n");
+      else
+        printf("Congratulations! You have guessed %u!\n", value);
+    }
+  while (guess != value);
 
-	return 0;
+  return 0;
 }
--- src/true_random.c
+++ src/true_random.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
-int true_random(int min, int max)
+int true_random(const int min, const int max)
 {
-	unsigned temp;
-	FILE *dev_random = fopen("/dev/random", "rb");
-	fread(&temp, sizeof(temp), 1, dev_random);
-	fclose(dev_random);
-	int result = temp % (max - min + 1) + min;
-	return result;
+  unsigned temp;
+  FILE *dev_random = fopen("/dev/random", "rb");
+  fread(&temp, sizeof(temp), 1, dev_random);
+  fclose(dev_random);
+  return temp % (max - min + 1) + min;
 }
